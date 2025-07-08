e64ca7fded671bdea6bcf3bb57671fa83cf9a5ee
woong <hw010624@naver.com>
2025-07-08 14:43:46 +0900

    feat : 프로젝트 생성

---
+++ src/test/java/com/woong/daangnmarket/DaangnmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woong.daangnmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
