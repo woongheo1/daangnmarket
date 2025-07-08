dc9634b0215361fbf8f9c7754abc3a17b52a6dc0
woong <152978780+woongheo1@users.noreply.github.com>
2025-07-08 22:46:48 +0900

    Revert "feat : feat: 회원가입 API 기능 구현"

--- src/main/java/com/woong/daangnmarket/member/domain/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.woong.daangnmarket.member.domain.repository;
-
-import com.woong.daangnmarket.member.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    boolean existsByEmail(String email);
-}
--- src/main/java/com/woong/daangnmarket/member/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.woong.daangnmarket.member.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
