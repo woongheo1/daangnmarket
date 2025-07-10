e1dbb7e0c6392011143be68d5d46bbf03024cd34
woong <hw010624@naver.com>
2025-07-10 15:04:13 +0900

    feat : JWT 로그인 및 토큰 발급 기능 구현

--- src/main/java/com/woong/daangnmarket/member/domain/repository/MemberRepository.java
+++ src/main/java/com/woong/daangnmarket/member/domain/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.woong.daangnmarket.member.domain.repository;
 import com.woong.daangnmarket.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
